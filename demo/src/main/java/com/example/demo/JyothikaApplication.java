15e34ae4eb0b0da24209ebe1b128bb085a8bcfff
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-08-10 22:57:14 +0530

    demo

---
+++ demo/src/main/java/com/example/demo/JyothikaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyothikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JyothikaApplication.class, args);
+	}
+
+}
